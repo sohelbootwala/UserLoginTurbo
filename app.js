c2ab55dcd6b8295c95e4910242e4c9345f800ecc
Sohel Bootwala <sohel.bootwala@gmail.com>
2018-01-16 11:52:11 -0600

    Create abilit for user

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Full Documentation - https://www.turbo360.co/docs
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 
 // initialize app
@@ -7,10 +6,12 @@ const app = vertex.app()
 // import routes
 const index = require('./routes/index')
 const api = require('./routes/api')
+const auth = require('./routes/auth')
 
 // set routes
 app.use('/', index)
-app.use('/api', api) // sample API Routes
+app.use('/api', api) 
+app.use('/auth', auth)
 
 
 module.exports = app
--- routes/auth.js
+++ routes/auth.js
@@ -1,25 +1,25 @@
-// Full Documentation - https://www.turbo360.co/docs
 const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
 
 /*  This is a sample API route. */
 
-router.get('/:resource', function(req, res){
-	res.json({
-		confirmation: 'success',
-		resource: req.params.resource,
-		query: req.query // from the url query string
-	})
-})
+router.post('/:register', function(req, res){
 
-router.get('/:resource/:id', function(req, res){
-	res.json({
-		confirmation: 'success',
-		resource: req.params.resource,
-		id: req.params.id,
-		query: req.query // from the url query string
-	})
+
+		turbo.createUser(req.body)
+		.then(data => {
+			res.json({
+				confirmation: 'success',
+				data: data
+			})
+		})
+		.catch(err => {
+				res.json({
+						confirmation: 'fail',
+						message: err.message
+				})
+		})
 })
 
 
