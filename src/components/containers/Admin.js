3fc152d9ea64d55ebf6dd8cb07cfc8a60539d1de
sohelbootwala <sohel.bootwala@gmail.com>
2018-01-24 16:17:04 -0600

    Return user info from backend to React component

--- src/components/containers/Admin.js
+++ src/components/containers/Admin.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
+import superagent from 'superagent'
 
 class Admin extends Component {
 
 		 componentDidMount(){
 		 		console.log('Admin component did mount')
+		 		//who is logged in???
+
+		 		superagent.get('/auth/currentuser')
+		 		.query(null)
+		 		.set('Accept', 'application/json')
+		 		.end((err, response) => {
+		 				if(err){
+		 						console.log('REQUEST ERROR' + err.message)
+		 							return
+		 				}
+
+		 				console.log('USER: ' + JSON.stringify(response.body))
+		 		})
 		 }
 
 		render(){
