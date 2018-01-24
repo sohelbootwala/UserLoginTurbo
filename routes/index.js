0a4773aac81abfe6f14de8b12f359db9d7373e24
sohelbootwala <sohel.bootwala@gmail.com>
2018-01-24 15:42:57 -0600

    Return entire logged in Users profile

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-// Full Documentation - https://www.turbo360.co/docs
 const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
