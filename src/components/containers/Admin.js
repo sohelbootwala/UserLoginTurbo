251f8c0936a0df16ac6d6a5cd02f3fe1baaca223
sohelbootwala <sohel.bootwala@gmail.com>
2018-01-24 16:04:50 -0600

    Create new component

---
+++ src/components/containers/Admin.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Admin extends Component {
+
+		
+
+		render(){
+				return (
+						<div className="container">
+								This is the Admind Container!
+						</div>
+						
+				)
+		}
+}
+
+export default Admin
